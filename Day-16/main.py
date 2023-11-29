d26e7ffb3c209cead927a8e56f5f6c35cf059ae9
Darian Schreuder <schreuderdb@gmail.com>
2023-11-29 14:09:38 +0200

    OOP Coffee_Machine

---
+++ Day-16/main.py
@@ -0,0 +1,24 @@
+from menu import Menu, MenuItem
+from money_machine import MoneyMachine
+from coffee_maker import CoffeeMaker
+
+while True:
+    
+    options = menu.get_items()
+    user_decision = input(f"What would you like? ({options}): ")
+
+    coffee_maker = CoffeeMaker()
+    money_machine = MoneyMachine()
+    menu = Menu()
+    
+    if user_decision == "report":
+        coffee_maker.report()
+        money_machine.report()
+    elif user_decision == "off":
+        print("powering down...")
+        break
+    else:
+        drink = menu.find_drink(user_decision)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)

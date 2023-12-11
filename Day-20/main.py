3b3ccadfe1bdeb5dc3f56da0c4a547a5ae7e2125
Darian Schreuder <schreuderdb@gmail.com>
2023-12-11 07:04:40 +0200

    Snake Game Progress

--- Day-20/main.py
+++ Day-20/main.py
@@ -7,6 +7,18 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-snake = Snake
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-game_is_one
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.05)
+    snake.move()
+
+screen.exitonclick()
